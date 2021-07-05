e94eabfcabc432cd71b1aeac75fdfb38753e2cbf
xiaoxin Hu <1033718596@qq.com>
2021-07-05 12:29:49 +0800

    less memory

--- en/unique_path_II.cpp
+++ en/unique_path_II.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
     int m;
     int n;
-    vector<vector<int>> myGrid;
+    vector<vector<int>> *myGrid;
     vector<vector<int>> record;
 
     int check_path(int row, int column){
@@ -15,10 +15,10 @@ class Solution {
         int next_row = row + 1;
         int next_column = column + 1;
         int total_path = 0;
-        if(next_row < m && myGrid[next_row][column] != 1){
+        if(next_row < m && (*myGrid)[next_row][column] != 1){
             total_path += check_path(next_row, column);
         }
-        if(next_column < n && myGrid[row][next_column] != 1){
+        if(next_column < n && (*myGrid)[row][next_column] != 1){
             total_path += check_path(row, next_column);
         }
         record[row][column] = total_path;
@@ -29,14 +29,14 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         m = obstacleGrid.size();
         n = obstacleGrid[0].size();
-        myGrid = obstacleGrid;
+        myGrid = &obstacleGrid;
         for(int i = 0;i < m;i++){
             record.push_back(vector<int>(n, -1));
         }
 
-        if(myGrid[m - 1][n - 1] == 1){return 0;}
+        if((*myGrid)[m - 1][n - 1] == 1){return 0;}
         else{record[m - 1][n - 1] = 1;}
-        if(myGrid[0][0] == 1){return 0;}
+        if((*myGrid)[0][0] == 1){return 0;}
         return check_path(0, 0);
     }
 };
