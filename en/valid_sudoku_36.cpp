216c2a908516a7439dfe93b0922e1ea7239ce416
xiaoxin Hu <1033718596@qq.com>
2021-07-06 12:02:01 +0800

    add comments

--- en/valid_sudoku_36.cpp
+++ en/valid_sudoku_36.cpp
@@ -2,6 +2,30 @@
 #include<ctype.h>
 using namespace std;
 
+/***
+ * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+ * 
+ * 
+ * 基本思路：遍历搜索
+ * 基本思想：暴力搜索每一行，每一列，每一个子格，查看是否重复。
+ * 优化：将搜索过的结果记录，记录方式以数字作为key，出现过的行、列、子格作为value。避免重复扫描。
+ * 如果出现过的任一达到两次以上，即为不合格。
+ * 
+ * 
+ * **/
+
 class Solution {
     // first 9 row second 9 column third 9 grid
     int digit_record[10][27] = {0};
@@ -18,7 +42,7 @@ class Solution {
     }
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        
+
         for(int i = 0;i < 9;i++){
             for(int j = 0;j < 9;j++){
                 if(isdigit(board[i][j])){
