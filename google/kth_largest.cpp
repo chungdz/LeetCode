90bf7368b28feb08773462d83ca0fa627a3f392e
xiaoxin Hu <1033718596@qq.com>
2021-08-25 09:57:56 +0800

    215

---
+++ google/kth_largest.cpp
@@ -0,0 +1,84 @@
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+/**
+ * 215
+ * 
+ * 
+ * 
+ * 
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 104
+-104 <= nums[i] <= 104
+ * 
+ * 
+ * 基本思路
+ * 
+ * sort
+ * 
+ * 或者用快速排序分治法找
+ * 
+ * 在运行过程中，直接sort效率更高，C++ sort的优化很好
+ * 
+ */
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        return nums[k - 1];
+    }
+};
+
+class Solution {
+public:
+    int quickSelect(vector<int>& a, int l, int r, int index) {
+        int q = randomPartition(a, l, r);
+        if (q == index) {
+            return a[q];
+        } else {
+            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
+        }
+    }
+
+    inline int randomPartition(vector<int>& a, int l, int r) {
+        int i = rand() % (r - l + 1) + l;
+        swap(a[i], a[r]);
+        return partition(a, l, r);
+    }
+
+    inline int partition(vector<int>& a, int l, int r) {
+        int x = a[r], i = l - 1;
+        for (int j = l; j < r; ++j) {
+            if (a[j] <= x) {
+                swap(a[++i], a[j]);
+            }
+        }
+        swap(a[i + 1], a[r]);
+        return i + 1;
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        srand(time(0));
+        return quickSelect(nums, 0, nums.size() - 1, nums.size() - k);
+    }
+};
