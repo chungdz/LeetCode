e6a9f06f22aae2afbcf6bf735dcc3bdc7fa7a43b
xiaoxin Hu <1033718596@qq.com>
2021-09-16 06:46:20 -0700

    17

--- google/Letter_Combinations_Phone.cpp
+++ google/Letter_Combinations_Phone.cpp
@@ -2,19 +2,39 @@
 
 17
 
-电话号码的字母组合
-给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
-给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
-
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
 
-示例:
 
-输入："23"
-输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
-说明:
-尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+ 
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+
+Constraints:
+
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+
+
+基本思想：
+
+暴力搜索
+不需要set去重
+
 */
 #include <map>
 #include <string>
@@ -22,12 +42,9 @@
 #include <set>
 using namespace std;
 
-/*
-暴力组合，set去重
-*/
 class Solution {
     map<char, vector<char>> phone_button;
-    set<string> results;
+    vector<string> results;
     int tail;
     string my_digits;
 public:
@@ -51,12 +68,12 @@ public:
         string origin = "";
         search_str(origin, 0);
 
-        return vector<string>(results.begin(), results.end());
+        return results;
     }
 
     void search_str(string pre, int pos){
         if(pos >= tail){
-            results.insert(pre);
+            results.push_back(pre);
             return;
         }
         
