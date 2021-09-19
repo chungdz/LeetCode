cff85a975a6a4db31d8a60510ee39b06d93c31f2
xiaoxin Hu <1033718596@qq.com>
2021-09-19 16:51:52 -0700

    interview 1 2

--- interview/google_interview_1.cpp
+++ interview/google_interview_1.cpp
@@ -13,9 +13,110 @@ using namespace std;
 /*
 
 
+There is a string S consisting of N letters 'a' and 'b'. In one move, a single letter can be removed. Removing the first or the last letter in the string costs 1 and removing any other letter costs 2. After removing a letter from the middle, the two remaining parts are joined.
 
+For example, given S = "abaab", removing the central letter 'a' from S costs 2. The string that would remain after this operation is "abab".
 
+What is the minimum cost needed to obtain a string without any letter 'b'?
 
+Write a function:
 
+class Solution { public int solution(String S); }
+
+that, given a string S, returns the minimum cost of removals so that the remaining sequence does not contain any letter 'b'.
+
+Examples:
+
+1. Given S = "aabaa", the function should return 2. After removing 'b' from the middle, the remaining sequence contains no 'b's.
+
+2. Given S = "abbaaba", the function should return 5. We can perform the following operations: abbaaba → bbaaba → baaba → aaba → aaa. The cost of these operations is 1 + 1 + 1 + 2 = 5. Another option is: abbaaba → bbaaba → baaba → aaba → aab → aa, resulting in a cost of 1 + 1 + 1 + 1 + 1 = 5.
+
+3. Given S = "bbb", the function should return 3. We can keep removing the last letter from the string until the string is empty.
+
+4. Given S = "abbbaabaabbba", the function should return 10. We can remove the middle letter 'b' (cost 2), then the first four letters ("abbb") and the last four letters ("bbba"), each at a cost of 4.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+string S consists only of the characters 'a' and/or 'b'.
+Keyboard navigation: Use Tab to advance the cursor. To exit the editor, press the ctrl and [ keys.
+1
+class Solution {
+2
+​
+3
+  public static int solution(String S) {
+4
+    System.err.println("Tip: Use System.err.println() to write debug messages on the output tab.");
+5
+    return 0;
+6
+  }
+7
+}
+8
 
 **/
+
+// abbaabab
+// 012345678
+// 1 0 2 1 0
+
+vector<int> b_pos;
+
+int min_path(){
+    int bs = b_pos.size();
+    if(bs == 2){
+        if(b_pos[0] >= 1 && b_pos[1] >= 1){
+            return 2;
+        }
+        return 1 + min(b_pos[0], b_pos[1]);
+    }
+
+    // from front
+    // method 1
+    int rec = b_pos[0];
+    b_pos.erase(b_pos.begin());
+    int m1 = 1 + rec + min_path();
+    // method 2
+    int rec_2 = b_pos[0];
+    b_pos[0] += rec;
+    int m2 = 2 + min_path();
+    // recover
+    b_pos[0] = rec_2;
+    b_pos.insert(b_pos.begin(), rec);
+
+    // from back
+    // method 3
+    int rec_back = b_pos[bs - 1];
+    b_pos.pop_back();
+    int m3 = 1 + rec_back + min_path();
+    // method 4
+    int rec_back_2 = b_pos[bs - 2];
+    b_pos[bs - 2] += rec_back;
+    int m4 = 2 + min_path();
+    // recover
+    b_pos[bs - 2] = rec_back_2;
+    b_pos.push_back(rec_back);
+
+    int m12 = min(m1, m2);
+    int m34 = min(m3, m4);
+
+    return min(m12, m34);
+}
+
+int solution(string S) {
+
+    int ss = S.size();
+    int pre_b = -1;
+    for(int i = 0;i < ss;++i){
+        if(S[i] == 'b'){
+            b_pos.push_back(i - pre_b - 1);
+            pre_b = i;
+        }
+    }
+    b_pos.push_back(ss - pre_b - 1);
+    
+    return min_path();
+}
+
--- interview/google_interview_2.cpp
+++ interview/google_interview_2.cpp
@@ -12,10 +12,49 @@ using namespace std;
 
 /*
 
+We are given an array A consisting of N integers. Find the maximum K (from 0 to N − 1) such that there exists a pair of positions (i, j) satisfying K = |i − j| = |A[i] − A[j]|, where |x| denotes absolute value of x. In other words, the distance between positions is equal to the difference between values. A position together with itself (when i = j) is always a valid pair for K = 0 (look at the third example).
 
+Write a function:
 
+class Solution { public int solution(int[] A); }
 
+that, given an array A of N integers, returns the maximum possible K.
 
+Examples:
+
+1. Given A = [2, 2, 2, 1], the function should return 1. The furthest valid pair is A[2] = 2 and A[3] = 1, as 1 = |2 − 3| = |2 − 1|.
+
+2. Given A = [2, 4, 6, 7, 4, 7, 2], the function should return 5. The furthest valid pair is A[0] and A[5].
+
+3. Given A = [100, 100, 100], the function should return 0. The only valid pairs are: A[0] and A[0], A[1] and A[1], A[2] and A[2].
+
+4. Given A = [1000000000], the function should return 0. The only valid pair is: A[0] and A[0].
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+
+each element of array A is an integer within the range [0..1,000,000,000].
 
 
 **/
+
+/// 2, 4, 6, 7, 4, 7, 2
+/// 0, 1, 2, 3, 4, 5, 6, 7
+/// 0
+
+int solution(vector<int> A){
+    int as = A.size();
+    int k = 0;
+    for(int i = 0;i < as - 1;++i){
+        int max_add = as - i - 1;
+        for(int lens = max_add;lens >= k + 1;--lens){
+            if(abs(A[i + lens] - A[i]) == lens){
+                k = lens;
+                break;
+            }
+        }
+    }
+
+    return k;
+}
