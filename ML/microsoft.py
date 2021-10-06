ccc79f06c97b70136defc81bbe6f5756d5b1c85e
xiaoxin Hu <1033718596@qq.com>
2021-10-05 19:46:37 -0700

    microsoft ml first interview

---
+++ ML/microsoft.py
@@ -0,0 +1,12 @@
+"""
+Seyi from microsoft outlook
+1. Go through a recent project objective / contribution / problem
+2. 当遇到noise problem的时候，怎么处理loss
+3. P-Value
+4. Supervised learning and Unsupervised Learning
+5. example and explain to a non-tech person
+6. sample number bias
+7. overfit
+8. 选择方向，最想去的最不想去的
+9. 你有什么问题
+"""
