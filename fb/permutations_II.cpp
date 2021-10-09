860a8ee6dc8fd0bb5f6a5ac59805c20abec23206
xiaoxin Hu <1033718596@qq.com>
2021-10-08 18:38:53 -0700

    47

--- fb/permutations_II.cpp
+++ fb/permutations_II.cpp
@@ -1,43 +1,43 @@
 #include<algorithm>
+#include<unordered_set>
 #include<vector>
 using namespace std;
 
 /*
-46
-Permutations
-Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+47
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
 
  
 
 Example 1:
 
-Input: nums = [1,2,3]
-Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
 Example 2:
 
-Input: nums = [0,1]
-Output: [[0,1],[1,0]]
-Example 3:
-
-Input: nums = [1]
-Output: [[1]]
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  
 
 Constraints:
 
-1 <= nums.length <= 6
+1 <= nums.length <= 8
 -10 <= nums[i] <= 10
-All the integers of nums are unique.
 
 基本思想
 
 把每个独特的数字放在最前，然后和n - 1的全permutation组合
+用set记录访问过的数字
 **/
 
 class Solution {
     vector<vector<int>> res;
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
         int ns = nums.size();
         search_p(ns, nums);
         return res;
@@ -49,7 +49,12 @@ public:
             return;
         }
         int cur_ta = clen - 1;
+        unordered_set<int> visited;
         for(int i = cur_ta;i >= 0;--i){
+            if(visited.find(nums[i]) != visited.end()){
+                continue;
+            }
+            visited.insert(nums[i]);
             swap(nums[cur_ta], nums[i]);
             search_p(cur_ta, nums);
             swap(nums[cur_ta], nums[i]);
