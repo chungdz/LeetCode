3f12057b35bf6039857cce2760a491cd6d49b536
xiaoxin Hu <1033718596@qq.com>
2021-10-15 17:14:04 -0700

    185

---
+++ fb/high3_in_dep.sql
@@ -0,0 +1,103 @@
+/*
+185 
+
+Department Top Three Salaries
+
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| Id           | int     |
+| Name         | varchar |
+| Salary       | int     |
+| DepartmentId | int     |
++--------------+---------+
+Id is the primary key column for this table.
+DepartmentId is a foreign key of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| Id          | int     |
+| Name        | varchar |
++-------------+---------+
+Id is the primary key column for this table.
+Each row of this table indicates the ID of a department and its name.
+ 
+
+A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+
+Write an SQL query to find the employees who are high earners in each of the departments.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 85000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
+| 7  | Will  | 70000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Joe      | 85000  |
+| IT         | Randy    | 85000  |
+| IT         | Will     | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+Explanation: 
+In the IT department:
+- Max earns the highest unique salary
+- Both Randy and Joe earn the second-highest unique salary
+- Will earns the third-highest unique salary
+
+In the Sales department:
+- Henry earns the highest salary
+- Sam earns the second-highest salary
+- There is no third-highest salary as there are only two employees
+
+row_number() over (partition by E2.DepartmentID order by E2.Salary desc)是关键
+注意为了保证distinct需要先嵌套一层
+*/
+
+SELECT D.Name as Department, E4.Name as Employee, E3.Salary
+FROM
+    (SELECT E2.Salary, E2.DepartmentID, 
+        row_number() over (partition by E2.DepartmentID order by E2.Salary desc) as S_rank
+    FROM
+        (SELECT DISTINCT E.Salary, E.DepartmentID
+        FROM Employee E) E2
+    ) E3, Department D, Employee E4
+WHERE E4.DepartmentID = D.Id
+AND E4.DepartmentID = E3.DepartmentID
+AND E4.Salary = E3.Salary
+AND E3.S_rank <= 3
+ORDER BY D.Name, S_rank DESC;
