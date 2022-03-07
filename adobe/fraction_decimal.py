a4dcc36482f2d971c114c84b53896d9984fefa7c
xiaoxin Hu <1033718596@qq.com>
2022-03-07 10:29:01 -0800

    166

---
+++ adobe/fraction_decimal.py
@@ -0,0 +1,69 @@
+'''
+166. Fraction to Recurring Decimal
+
+Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+
+If the fractional part is repeating, enclose the repeating part in parentheses.
+
+If multiple answers are possible, return any of them.
+
+It is guaranteed that the length of the answer string is less than 104 for all the given inputs.
+
+ 
+
+Example 1:
+
+Input: numerator = 1, denominator = 2
+Output: "0.5"
+Example 2:
+
+Input: numerator = 2, denominator = 1
+Output: "2"
+Example 3:
+
+Input: numerator = 4, denominator = 333
+Output: "0.(012)"
+ 
+
+Constraints:
+
+-2^31 <= numerator, denominator <= 2^31 - 1
+denominator != 0
+
+基本思想
+
+长除法，分类处理
+处理符号，整数位，小数位
+记录小数位的余数，如果出现重复的余数，就找到之前余数出现的位置左边加括号
+'''
+
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        if numerator % denominator == 0:
+            return str(numerator // denominator)
+
+        s = []
+        if (numerator < 0) != (denominator < 0):
+            s.append('-')
+
+        # 整数部分
+        numerator = abs(numerator)
+        denominator = abs(denominator)
+        integerPart = numerator // denominator
+        s.append(str(integerPart))
+        s.append('.')
+
+        # 小数部分
+        indexMap = {}
+        remainder = numerator % denominator
+        while remainder and remainder not in indexMap:
+            indexMap[remainder] = len(s)
+            remainder *= 10
+            s.append(str(remainder // denominator))
+            remainder %= denominator
+        if remainder:  # 有循环节
+            insertIndex = indexMap[remainder]
+            s.insert(insertIndex, '(')
+            s.append(')')
+
+        return ''.join(s)
