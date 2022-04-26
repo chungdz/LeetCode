bf01093e5e908a1efd9dce78b259e1c2a214fb4e
xiaoxin Hu <1033718596@qq.com>
2022-04-26 14:54:35 -0700

    meta rds

---
+++ interview/meta_rds.py
@@ -0,0 +1,32 @@
+'''
+
+intervew 1
+
+A 带空格的sorted list
+B sorted list
+merge B into A
+
+'''
+# 指向B
+i = 0 
+# 指向A
+j = lenb
+# 指向A的head
+k = 0
+# 遍历i和j，填充k，把j遍历完
+i = 0
+while b < lenb:
+    if B[b] <= A[a]:
+        A[i] = B[b]
+        b += 1
+    else:
+        A[i] = A[a]
+        a += 1
+    i += 1
+
+for k in d1:
+    if k in d2:
+        total += d1[k] + d2[k]
+
+
+
