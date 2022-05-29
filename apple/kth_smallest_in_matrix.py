70e77faa945895c8ff4036f54f0b8a8649250a7b
xiaoxin Hu <1033718596@qq.com>
2022-05-29 15:45:06 -0700

    378

---
+++ apple/kth_smallest_in_matrix.py
@@ -0,0 +1,85 @@
+import heapq
+
+'''
+378 Kth Smallest Element in a Sorted Matrix
+
+Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+You must find a solution with a memory complexity better than O(n2).
+
+ 
+
+Example 1:
+
+Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+Output: 13
+Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+Example 2:
+
+Input: matrix = [[-5]], k = 1
+Output: -5
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 300
+-109 <= matrix[i][j] <= 109
+All the rows and columns of matrix are guaranteed to be sorted in non-decreasing order.
+1 <= k <= n2
+ 
+
+Follow up:
+
+Could you solve the problem with a constant memory (i.e., O(1) memory complexity)?
+Could you solve the problem in O(n) time complexity? The solution may be too advanced for an interview but you may find reading this paper fun.
+
+基本思想
+
+k路归并排序，用最小堆存储n个值，每次加入一列，然后不停弹出栈，直到弹了k-1个数为止
+
+思想二：
+二分走格子，时间更长，但是空间更小
+'''
+
+class Solution1:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+        pq = [(matrix[i][0], i, 0) for i in range(n)]
+        heapq.heapify(pq)
+
+        ret = 0
+        for i in range(k - 1):
+            num, x, y = heapq.heappop(pq)
+            if y != n - 1:
+                heapq.heappush(pq, (matrix[x][y + 1], x, y + 1))
+        
+        return heapq.heappop(pq)[0]
+
+
+class Solution2:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+
+        def check(mid):
+            i, j = n - 1, 0
+            num = 0
+            while i >= 0 and j < n:
+                if matrix[i][j] <= mid:
+                    num += i + 1
+                    j += 1
+                else:
+                    i -= 1
+            return num >= k
+
+        left, right = matrix[0][0], matrix[-1][-1]
+        while left < right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
